6b447e73b021a3a5da8287efe64a615bc771f260
AdrianMartinTiscar <matiad_z@hotmail.com>
2021-03-03 16:33:39 +0100

    main añadido

--- Tiny0/Tiny0/src/analizador/AnalizadorLexicoTiny.java
+++ Tiny0/Tiny0/src/analizador/AnalizadorLexicoTiny.java
@@ -2,4 +2,21 @@ package analizador;
 
 public class AnalizadorLexicoTiny {
 
+	
+	
+	
+	
+	 public static void main(String arg[]) throws IOException {
+		 
+		 
+		 
+	    /* Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+	     AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
+	     UnidadLexica unidad;
+	     do {
+	       unidad = al.sigToken();
+	       System.out.println(unidad);
+	     }
+	     while (unidad.clase() != ClaseLexica.EOF);*/
+	 } 
 }
