ee84b6787ef91635a63b7ac67d0124de8091afe2
AdrianMartinTiscar <matiad_z@hotmail.com>
2021-04-13 21:30:25 +0200

    fase2(tiny1 v0.1)

---
+++ Fase2/Tiny1/ASTiny1/src/descendente/Main.java
@@ -0,0 +1,8 @@
+package descendente;
+import java.io.FileReader;
+public class Main{
+   public static void main(String[] args) throws Exception {
+      AnalizadorSintacticoTiny1 asint = new AnalizadorSintacticoTiny1(new FileReader(args[0]));
+	  asint.Programa();
+   }
+}
