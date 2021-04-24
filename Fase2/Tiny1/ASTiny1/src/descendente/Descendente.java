b70de38788c3297160d4f9e53f84692812d4a333
AdrianMartinTiscar <matiad_z@hotmail.com>
2021-04-24 14:27:53 +0200

    FUNCIONA TODOOO!!

---
+++ Fase2/Tiny1/ASTiny1/src/descendente/Descendente.java
@@ -0,0 +1,21 @@
+package descendente;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+public class Descendente{
+	private String fich;
+   public Descendente(String fich){
+	   this.fich = fich;
+   }
+   
+   public void init() throws FileNotFoundException {
+	   AnalizadorSintacticoTiny1 asint = new AnalizadorSintacticoTiny1(new FileReader(fich));
+	   try{
+		   asint.ProgramaPrev();
+		   System.out.println("OK");
+	   }
+	   catch(Exception e) {
+		   System.out.println(e.getMessage());
+	   }
+	   
+   }
+}
---
+++ Fase2/Tiny1/ASTiny1/src/init/Main.java
@@ -0,0 +1,27 @@
+package init;
+
+import conCup.asint.*;
+import descendente.Descendente;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.out.println(
+					"Argumentos requerridos 2: i archivo a analizar; ii desc (analisis descendente)/ asc (analisis ascendente).");
+		} else {
+			System.out.println("Resultado del analizador " + args[1] + ":");
+			if (args[1].equals("desc")) {
+				Descendente des = new Descendente(args[0]);
+				des.init();
+			} else if (args[1].equals("asc")) {
+				Ascendente asc = new Ascendente(args[0]);
+				asc.init();
+			} else
+				System.out.println(
+						"El segundo argumento debe ser desc (analisis descendente) o asc (analisis ascendente).");
+		}
+
+	}
+
+}
