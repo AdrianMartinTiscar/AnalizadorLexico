85ac81d693bfb0b55d61f4bcda34d1c023f1a41d
AdrianMartinTiscar <matiad_z@hotmail.com>
2021-05-30 18:33:11 +0200

    sinerrores

--- Fase2/Tiny1/ASTiny1/src/init/Main.java
+++ Fase2/Tiny1/ASTiny1/src/init/Main.java
@@ -7,37 +7,42 @@ import conCup.alex.AnalizadorLexicoTiny;
 import conCup.asint.AnalizadorSintacticoTiny;
 
 import conCup.asint.*;
-import descendente.Descendente;
+import descendente.ConstructorAST1;
 import procesamiento.Impresion;
 import procesamiento.TinyASint.Prog;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		/*if (args.length != 2) {
+		if (args.length != 2) {
 			System.out.println(
 					"2 argumentos requeridos: I: archivo a analizar; II: desc (analisis descendente)/ asc (analisis ascendente).");
 		} else {
 			System.out.println("Resultado del analizador " + args[1] + ":");
+			Prog pro = null;
 			if (args[1].equals("desc")) {
-				Descendente des = new Descendente(args[0]);
-				des.init();
+				pro = ejecuta_descendente(args[0]);
 			} else if (args[1].equals("asc")) {
-				Ascendente asc = new Ascendente(args[0]);
-				asc.init();
+				pro = ejecuta_ascendente(args[0]);
 			} else
 				System.out.println(
 						"El segundo argumento debe ser desc (analisis descendente) o asc (analisis ascendente).");
-		}*/
-		
-		Reader input = new InputStreamReader(new FileInputStream(args[0]));
-	     AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
-	     AnalizadorSintacticoTiny constructorast = new AnalizadorSintacticoTiny(alex);
-	     Prog p = (Prog) constructorast.parse().value;
-	     
-	     int a = 1;
-	     p.procesa(new Impresion());
-	     //constructorast.debug_parse();
+		pro.procesa(new Impresion());
+		// constructorast.debug_parse();
+		}		
+	}
+
+	private static Prog ejecuta_ascendente(String in) throws Exception {
+		Reader input = new InputStreamReader(new FileInputStream(in));
+		AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+		AnalizadorSintacticoTiny constructorast = new AnalizadorSintacticoTiny(alex);
+		return (Prog) constructorast.parse().value;
+	}
+
+	private static Prog ejecuta_descendente(String in) throws Exception {
+		Reader input = new InputStreamReader(new FileInputStream(in));
+		descendente.ConstructorAST1 constructorast = new descendente.ConstructorAST1(input);
+		return constructorast.ProgramaPrev();
 	}
 
 }
